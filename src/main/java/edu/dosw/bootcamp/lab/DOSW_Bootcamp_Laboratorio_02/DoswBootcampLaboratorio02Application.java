7e63d49d1b03a63b854581503b37523ba9d375bd
Oscar10lm <oscar.lasso-m@mail.escuelaing.edu.co>
2026-08-11 15:50:37 -0500

    Reestructuracion de paquetes

---
+++ src/main/java/edu/dosw/bootcamp/lab/DOSW_Bootcamp_Laboratorio_02/DoswBootcampLaboratorio02Application.java
@@ -0,0 +1,13 @@
+package edu.dosw.bootcamp.lab.DOSW_Bootcamp_Laboratorio_02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoswBootcampLaboratorio02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoswBootcampLaboratorio02Application.class, args);
+	}
+
+}
