2610c83c6bf3167483a4412c6df9cc2f8900cd80
PaulaDiaz <paula.diaz-a@mail.escuelaing.edu.co>
2026-08-11 22:21:04 -0500

    Fase 3

--- src/main/java/edu/dosw/bootcamp/lab/DoswBootcampLaboratorio02Application.java
+++ src/main/java/edu/dosw/bootcamp/lab/DoswBootcampLaboratorio02Application.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DoswBootcampLaboratorio02Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DoswBootcampLaboratorio02Application.class, args);
+
+        SpringApplication.run(DoswBootcampLaboratorio02Application.class, args);
 	}
 
 }
